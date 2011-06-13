eaab2ca6903ec58d0d8e8e4a5149f016de50b081
Myron Turner <turnermm02@shaw.ca>
2011-06-13 11:15:55 -0500

    Moved substitute jQuery class for tw-sack from helper.php to script.js.

--- fckg/helper.php
+++ fckg/helper.php
@@ -183,31 +183,6 @@ var ourLockTimerINI = false;
  }
 
 
-  if(!window.jQuery) {
-     var jQuery = {
-      ajax: function(obj) {
-         var s = new sack(obj.url); 
-         s.asynchronous = obj.async;
-         s.onCompletion = function() {
-        	if (s.responseStatus &&s.responseStatus[0] == 200) {   
-                  obj.success(s.response);
-        	}
-         };
-         s.runAJAX(obj.data);
-     
-      },
-      post: function(url,params,callback,context) {
-         var s = new sack(url);
-         s.onCompletion = function() {
-        	if (s.responseStatus &&s.responseStatus[0] == 200) {   
-                  callback(s.response);
-        	}
-         };
-         s.runAJAX(params);
-      }
-     };
-  }
-
  function lockTimerRefresh(bak) {
         var now = new Date();
         if(!ourLockTimerINI)  unsetDokuWikiLockTimer();
--- fckg/script.js
+++ fckg/script.js
@@ -241,5 +241,31 @@ function dwedit_draft_delete(cname) {
 
 }
 
+
+  if(!window.jQuery) {
+     var jQuery = {
+      ajax: function(obj) {
+         var s = new sack(obj.url); 
+         s.asynchronous = obj.async;
+         s.onCompletion = function() {
+        	if (s.responseStatus &&s.responseStatus[0] == 200) {   
+                  obj.success(s.response);
+        	}
+         };
+         s.runAJAX(obj.data);
+     
+      },
+      post: function(url,params,callback,context) {
+         var s = new sack(url);
+         s.onCompletion = function() {
+        	if (s.responseStatus &&s.responseStatus[0] == 200) {   
+                  callback(s.response);
+        	}
+         };
+         s.runAJAX(params);
+      }
+     };
+  }
+
 var dokuBase = location.host + DOKU_BASE;
 
