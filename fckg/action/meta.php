4d1afdc08e4e3c3ac4a8f75f979e9a5765ad2013
Myron Turner <turnermm02@shaw.ca>
2011-09-04 14:06:14 -0500

    minor editor to meta.php

--- fckg/action/meta.php
+++ fckg/action/meta.php
@@ -316,7 +316,9 @@ function fck_editor(&$event) {
     //<![CDATA[ 
 
     try {
-    if(!HTMLParserInstalled){};
+    if(!HTMLParserInstalled){
+	    LoadScript("$url");  
+		}
     }
     catch (ex) {  
          LoadScript("$url");        
